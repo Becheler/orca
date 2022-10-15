c1e908208f5e90fdd340c5478218d9a80652a1f7
arnaud <arnaud@irreverentlabs.com>
2022-10-15 12:59:16 -0700

    tests build and run and fail

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # orca
 Orbit Counting Algorithm
 
-Build the project using conan dependency managemer
+Build the project using conan dependency manager and run test
 ```
 python3 -m venv virtual-env
 source virtual-env/bin/activate
@@ -9,4 +9,6 @@ pip install conan==1.49.0
 conan install conan/conanfile.txt --build=missing --install-folder=build
 cd build
 cmake -D CMAKE_TOOLCHAIN_FILE=conan_paths.cmake ..
+cmake --build .
+ctest
 ```
--- tests/unit_test/graphlets.cpp
+++ tests/unit_test/graphlets.cpp
@@ -1,17 +1,14 @@
 
-#define BOOST_TEST_MODULE TestHybridLambda
+#define BOOST_TEST_MODULE TestORCA
 
 #include <boost/test/unit_test.hpp>
-
-#include <hybrid_lambda_lib/hybridLambda.hpp>
-#include "hybrid_lambda_lib/sim_gt.hpp"
 
 BOOST_AUTO_TEST_SUITE( assert_not_throws )
 
 BOOST_AUTO_TEST_CASE( test_command_line_interface )
 {
-  BOOST_ASSERT_EQUAL(1,1);
-  BOOST_ASSERT_EQUAL(1,2);
+  BOOST_CHECK_EQUAL(1,1);
+  BOOST_CHECK_EQUAL(1,2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
